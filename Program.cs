268edcdb6c0bfc425b6ee0e1ff22b1319bc60027
student <nikas.artes@gmail.com>
2017-02-14 12:24:08 +0500

    class Man was made

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication16
 {
+    class Man
+    {
+        public string name;
+        public int age;
+    }
     class Program
     {
         static void Main(string[] args)
