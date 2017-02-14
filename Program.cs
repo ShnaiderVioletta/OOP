ae4a3645412ced9b8502a5483d75b44170cb1be0
student <nikas.artes@gmail.com>
2017-02-14 12:56:36 +0500

    something new
    constructor, get, set

--- Program.cs
+++ Program.cs
@@ -6,16 +6,56 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication16
 {
+    class Point
+    {
+        public int X;
+        public int Y;
+    }
     class Man
     {
         public string name;
-        public int age;
+        public int age 
+        { 
+            get { return age; }
+            set 
+            { 
+                if((age>=0)&&(age<100))
+                    age = value; 
+            } 
+        }
+        private int money;
+        public Point Geolocation { get; set; }
+        public Man()
+        {
+            Console.WriteLine("I was born!");
+            name = "";
+            age = 0;
+            money = 0;
+        }
+
+        public Man(string tmpName, int tmpAge, int tmpMoney)
+        {
+            age = tmpAge;
+            name = tmpName;
+            money = tmpMoney;
+        }
+
+        public void SetMoney(int tmpMoney)
+        {
+            money = tmpMoney;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Man person = new Man("Vanya", 30, 10);
+            person.SetMoney(20);
+            Point tmppoint = new Point();
+            tmppoint.X = 10;
+            tmppoint.Y = 15;
 
+            person.Geolocation = tmppoint;
         }
     }
 }
